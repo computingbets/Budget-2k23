d064f76cf22978a9eb1ee3ec6b76f17802ad6489
John Staley <staley77@protonmail.com>
2022-10-06 07:11:46 -0400

    add NewExpense.css, NewExpense. modify ExpenseItem.js for clarity

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,27 @@
 import { useCallback } from "react";
-import ExpenseDate from './ExpenseDate';
+import { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import ExpensePrice from "./ExpensePrice";
 import Card from "../UI/Card";
 
-
 function ExpenseItem(props) {
+  //state variable and what Max dubs, Updating Function. 
+  //useState holds the initial state and pooUpdate holds the new state
+  const [poop, pooUpdate] = useState(props.title);
+  const clickHandler = () => {
+    pooUpdate("It's Groundhog Day!!!");
+    };
+
   return (
-     <Card className="expense-item">
-      <ExpenseDate date = {props.date} />
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{poop}</h2>
         <ExpensePrice amount={props.amount} />
       </div>
-      </Card>
+      <button onClick={clickHandler}>change title</button>
+    </Card>
   );
 }
 
