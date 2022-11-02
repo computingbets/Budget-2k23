38ff6713777397371bb344823452dbdd235009b9
John Staley <staley77@protonmail.com>
2022-11-01 22:16:46 -0400

    pass data from child to parent and store with state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,13 +2,19 @@ import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import "./ExpensePrice.css";
 import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
+
 
 const Expenses = function (props) {
-    console.log(props)
+  const [year, changeYear] = useState(2020);
+
+   const yearHandler = dataYear => {
+      changeYear(dataYear);
+    };
     return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter />
+        <ExpensesFilter selected={year} onChangeFilter={yearHandler} />
       <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
       <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
       <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,14 +1,21 @@
 import React from 'react';
-
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
+//wrap event handler in a Component. Looking to store state variable and 
+//pass data up to Expenses. Through props, yes. So, pre-solution code. 
+
+const ExpensesFilter = (props) => {
+  
+  const filterDateHandler = (event) => {
+    //console.log(event.target.value)
+    props.onChangeFilter(event.target.value);
+    };
 
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select value={props.selected} onChange={filterDateHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
