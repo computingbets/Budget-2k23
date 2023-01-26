38dc0884b9fec8a314ab408aa62a8ca15d3a2ce5
John Staley <staley77@protonmail.com>
2023-01-26 10:50:06 -0500

    add dynamic styling in ChartBar.js Component

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -4,7 +4,25 @@ import "./ChartBar.css";
 
 
 const ChartBar = (props) => {
-    return (
+    let barBarometer = '0%';
+
+    // computing dynamic css values
+    if (props.maxValue > 0) {
+        barBarometer = Math.round((props.value / props.maxValue) * 100) + "%";
+    }
+
+    return (<div className="chart-bar">
+                <div className="chart-bar__inner">
+
+                    <div className="chart-bar__fill" style={{height: barBarometer}}></div>
+                    </div>
+
+                    <div className="chart-bar__label">{props.metric}</div>
+
+            </div>
+
 
     )
 } 
+
+export default ChartBar;
